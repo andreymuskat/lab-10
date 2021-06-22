22a75d560217ccb45454a251edd7ffb7a8dd0d5d
andreymuskat <andreymuskat@yandex.ru>
2021-06-22 03:15:13 +0100

    all lab

---
+++ cmake-build-debug/CMakeFiles/template.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/andrey/CLionProjects/labs10/sources/Dbcs.cpp" "/home/andrey/CLionProjects/labs10/cmake-build-debug/CMakeFiles/template.dir/sources/Dbcs.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../third-party/PicoSHA2"
+  "/home/andrey/.hunter/_Base/35fa55b/252be92/5a6bcbe/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/andrey/CLionProjects/labs10
+# Build directory: /home/andrey/CLionProjects/labs10/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/home/andrey/CLionProjects/labs10/cmake-build-debug/tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/andrey/CLionProjects/labs10/CMakeLists.txt;79;add_test;/home/andrey/CLionProjects/labs10/CMakeLists.txt;0;")
